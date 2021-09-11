7af480663fc145e08146d917c2937f0a354ca2c2
Nureddin Elmas <nureddinelmas34@gmail.com>
2021-09-11 13:11:55 +0200

    Added Quiz and Result Activity

---
+++ app/src/main/java/com/nureddinelmas/myquiz/ResultActivity.kt
@@ -0,0 +1,11 @@
+package com.nureddinelmas.myquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
